fc19d87a77a32b9346eff3499fc973e0495d37c2
kimdonguke <rlaehddnr100@gmail.com>
2019-12-27 05:19:54 +0900

    viewpager, recyclered view, add item

--- app/src/main/java/com/example/sojunrecycleredview/fragment_list.java
+++ app/src/main/java/com/example/sojunrecycleredview/fragment_list.java
@@ -4,6 +4,7 @@ package com.example.sojunrecycleredview;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,13 +15,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 public class fragment_list extends Fragment {
-    RecyclerView recyclerView;
-    RecyclerViewAdapter recyclerViewAdapter;
+    Button add_button;
+    EditText editText_howmuch,editText_what;
+    ImageButton camera_button;
+    ArrayList<Item> data=new ArrayList<>();
     @Override
     public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
@@ -29,18 +35,25 @@ public class fragment_list extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Context context=getContext();
         View v= inflater.inflate(R.layout.activity_fragment_list, container, false);
-        ArrayList<Item> data=new ArrayList<>();
+
 //        data.add(addItem(getResources().getDrawable(R.drawable.gomtang),"gimoring1","fuckyou"));
 //        data.add(addItem(getResources().getDrawable(R.drawable.honor_3level),"gimoring2","fuckyou"));
 //        data.add(addItem(getResources().getDrawable(R.drawable.kimminjun),"gimoring3","fuckyou"));
         RecyclerView recyclerView=v.findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        SimpleTextAdapter simpleTextAdapter=new SimpleTextAdapter(data);
+        final SimpleTextAdapter simpleTextAdapter=new SimpleTextAdapter(data);
         recyclerView.setAdapter(simpleTextAdapter);
-
-
-
-
+        add_button=v.findViewById(R.id.fragment_list_addbutton);
+        editText_howmuch=v.findViewById(R.id.fragment_list_howmuch);
+        editText_what=v.findViewById(R.id.fragment_list_what);
+        camera_button=v.findViewById(R.id.fragment_list_camerabutton);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data.add(addItem(camera_button.getDrawable(),editText_howmuch.getText().toString(),editText_what.getText().toString()));
+                simpleTextAdapter.notifyDataSetChanged();
+            }
+        });
         return  v;
     }
     public Item addItem(Drawable icon, String title, String desc){
